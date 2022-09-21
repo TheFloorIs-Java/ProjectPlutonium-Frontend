895089370ac416f729006c4dcf12649b7fde8a8b
programjourney1 <programjourney1@gmail.com>
2022-09-21 17:37:59 -0400

    corey add admin

--- src/app/pages/admin-page-component/admin-page-component.component.ts
+++ src/app/pages/admin-page-component/admin-page-component.component.ts
@@ -13,3 +13,29 @@ export class AdminPageComponentComponent implements OnInit {
   }
 
 }
+//////////////////////Button click to load users
+let button = document.getElementById('loadButton');
+button.onclick = loadUsers;
+function loadUsers(){
+  let request = new XMLHttpRequest();
+  request.open("GET", "http://localhost:9004/Users");
+  request.send();
+
+  request.onreadystatechange = load;
+
+  function load(){
+      if(request.readyState == 4 && request.status == 200){
+          let responsejson = JSON.parse(request.response);
+          console.log(responsejson);
+          loadSpan(responsejson);
+      }
+  }
+}
+///////for loop to dispaly
+function loadSpan(response){
+
+  for(let i = 0; i < response.length; i++){
+      let pokemon = document.createElement("p");
+      pokemon.innerText = response[i].Users +" -";
+      content.appendChild(pokemon);
+  }}
