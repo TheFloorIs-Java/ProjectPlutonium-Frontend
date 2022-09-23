4d0c2f0fb0b50667340174b782e9f567bc847870
MattHill92 <matthew765@revature.net>
2022-09-23 15:27:05 -0700

    login form post request built

--- src/app/component/login-form-component/login-form-component.component.ts
+++ src/app/component/login-form-component/login-form-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-form-component',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http :HttpClient) { }
 
+  usernameInput:any = "";
+  passwordInput:any = "";
+  errorMessage: string = "";
+
+  
   ngOnInit(): void {
   }
 
+  loginButtonPressed(){
+    if (this.usernameInput.trim().length === 0){
+      this.errorMessage = "Username Required";
+    }
+    else if (this.passwordInput.trim().length === 0){
+      this.errorMessage = "Password Required";
+    }
+    else {
+      this.errorMessage = "";
+      this.usernameInput = "";
+      this.passwordInput = "";
+
+      const headers = { 'Authorization': 'Bearer my-token', 'My-Custom-Header': 'foobar' }
+      this.http.post("https://projectplutonium.azurewebsites.net/login", 
+      {
+        username: this.usernameInput,
+        password: this.passwordInput
+      },
+      {responseType: 'text'}
+        ).subscribe(data=>console.log(data));
+    }
+  }
 }
