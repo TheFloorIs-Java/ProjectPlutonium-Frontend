56e3579b690c45c44e03e952c9553b266ecbf350
MattHill92 <matthew765@revature.net>
2022-09-26 15:41:54 -0700

    adjusted user

--- src/app/Model/user.ts
+++ src/app/Model/user.ts
@@ -5,8 +5,10 @@ export interface user {
 
     id : number;
     username : String;
+    password : String;
     permissionLevel : number;
+    salt : String;
     sessionId : String;
-
+    sessionExpiration : Date;
 
 }
