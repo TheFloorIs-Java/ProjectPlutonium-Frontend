9c53fcb4dd145c0dcef21ca37003a07a86df3fec
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-09-26 21:47:38 -0500

    worked on user page, created game interface, working on displaying games made

--- src/app/component/profile-info/profile-info.component.ts
+++ src/app/component/profile-info/profile-info.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileInfoComponent implements OnInit {
 
+  username : String = this.getUsername();
+  url : String = this.getProfilePicture();
+  bio : String = this.getBio();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getUsername() : String {
+    return "PlaceholderUsername";
+  }
+
+  getProfilePicture() : String {
+    return "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+  }
+
+  getBio() : String {
+    return "Placeholder Bio";
+  }
 }
--- src/app/component/profile-stats/profile-stats.component.ts
+++ src/app/component/profile-stats/profile-stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'app/Model/game';
 
 @Component({
   selector: 'app-profile-stats',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileStatsComponent implements OnInit {
 
+  placeholderGamesMade : Array<Game> = [
+    {
+      title: "Game 1",
+      gameID: 1,
+      userID: 1
+    },
+    {
+      title: "Game 2",
+      gameID: 2,
+      userID: 1
+    },
+    {
+      title: "Game 3",
+      gameID: 3,
+      userID: 1
+    }
+  ]
+
+  //gamesmade: Array<String> = this.getGamesMade(); 
+  score : number = this.getScore();
+  rank : number = this.getRank();
+  avgscore : number = this.getAvgScore();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getScore() : number {
+    return 5;
+  }
+
+  getRank() : number {
+    return 1;
+  }
+
+  getAvgScore() : number {
+    return 6;
+  }
+
+  /*getGamesMade(): Array<String> {
+    for (let i = 0; i< this.placeholderGamesMade.length; i++) {
+      this.gamesmade[i] = this.placeholderGamesMade[i].title;
+    }
+    return this.gamesmade;
+  }
+*/
 }
