6ce03f0c6110fae75ea11db0a2b72b50a18e8afd
Natalia <natalia.molla222@gmail.com>
2022-09-27 13:36:17 -0700

    added post scorecard and login redirect

--- src/app/component/zombie-game/zombie-game.component.ts
+++ src/app/component/zombie-game/zombie-game.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {scene} from '../../Model/scene';
 import { actionbutton } from 'app/Model/actionbutton';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import {user} from '../../Model/user';
+import { publishedGame } from 'app/Model/publishedGame';
 
 @Component({
   selector: 'app-zombie-game',
@@ -12,11 +16,19 @@ import { actionbutton } from 'app/Model/actionbutton';
 
 export class ZombieGameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http :HttpClient, private cookieService : CookieService) { }
  
 
   @Input()
   data : Array<scene> = [];
+
+  @Input()
+  user : user = {}as user;
+
+  @Input()
+  publishedGame : publishedGame =  {}as publishedGame;
+
+ 
 
 
   description : String = "";
@@ -26,19 +38,45 @@ export class ZombieGameComponent implements OnInit {
   actions : Array<actionbutton> = [];
 
   gamename : String = "Zombie Survival";
+  
+  killcount : number = 0;
+  
+  
+ 
 
   
 
   ngOnInit(): void {
-    this.changePage(0);
+    this.changePage(0,0);
   }
 
-  changePage(sceneindex:number) {
+  changePage(sceneindex:number, killcount:number) {
 
     this.description = this.data[sceneindex].description;
     this.map = this.data[sceneindex].map;
     this.actions = this.data[sceneindex].actions;
+    this.killcount += killcount;
 
   }
+
+  postScoreOnComplete(user : user, killcount: number, publishedGame : publishedGame) {
+
+    this.http.post("https://projectplutonium.azurewebsites.net/scoreCard",
+    
+    {
+      'user' : user.username,
+      'publishedGame' : publishedGame.id,
+      'date' : publishedGame.id,
+      'score' : killcount,
+
+
+    },{
+      headers:{session: this.cookieService.get("session") },
+
+    })
+
+  }
+
+  
 
 }
