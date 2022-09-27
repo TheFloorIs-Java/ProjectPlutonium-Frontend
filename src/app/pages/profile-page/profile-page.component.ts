c3d501c7c9a2f8c460c82361ca5151af50674a0e
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-09-26 22:21:51 -0500

    Started profile init logic, trouble grabbing query param

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+//import 'rxjs/add/operator/filter';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  
+  id : number = 0;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    /*this.route.queryParams
+      .filter(params => params.id)
+      .subscribe(params => {
+        console.log(params); 
+
+        this.id = params.id;
+
+        console.log(this.id);
+      }
+    );
+    */
+
+    if (this.id = 0) {
+      //load page with user info from param
+    } else {
+      //check to see if user is logged in
+      this.http.get("http://projectplutonium.azurewebsites.net/users/session",
+      {
+        responseType : 'json'
+      }
+      ).subscribe(data =>console.log(data));
+    }
+
   }
+
+  
 
 }
