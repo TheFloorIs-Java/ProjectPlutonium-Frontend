39d458396460894156ece0626b3dc34141208383
programjourney1 <programjourney1@gmail.com>
2022-10-06 16:43:25 -0400

    FIXED Jasmine

--- src/app/component/profile-games-created/profile-games-created.component.spec.ts
+++ src/app/component/profile-games-created/profile-games-created.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProfileGamesCreatedComponent } from './profile-games-created.component';
 
 
-fdescribe('ProfileGamesCreatedComponent', () => {
+describe('ProfileGamesCreatedComponent', () => {
   let component: ProfileGamesCreatedComponent;
   let fixture: ComponentFixture<ProfileGamesCreatedComponent>;
 
